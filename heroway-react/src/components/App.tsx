ac73ec8dacf034b9486ad42f2655795b4e6e9db7
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-02-01 09:17:37 -0300

    create hello world component

--- heroway-react/src/components/App.tsx
+++ heroway-react/src/components/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HelloWorld from './HelloWorld/HelloWorld';
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <HelloWorld />
         </p>
         <a
           className="App-link"
---
+++ heroway-react/src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HelloWorld() {
+  return (
+    <h1>Hello World my name is Marcio</h1>
+  );
+}
+
+export default HelloWorld;
